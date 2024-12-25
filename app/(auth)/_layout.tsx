c27a4b71b46f288152a4d035f078a32f81eb6c2e
Enes Demirci <20194604004@ogr.akdeniz.edu.tr>
2024-12-25 23:53:37 +0300

    5: Enhance login screen layout and functionality; add header with navigation and input fields for email and password

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,10 +1,27 @@
-import { Stack } from "expo-router";
+import images from "@/constants/images";
+import { Link, Stack } from "expo-router";
+import { Image } from "react-native";
 
 const AuthLayout = () => {
   return (
     <Stack>
       <Stack.Screen name="welcome" options={{ headerShown: false }} />
-      <Stack.Screen name="login" options={{ headerShown: false }} />
+      <Stack.Screen
+        name="login"
+        options={{
+          headerShown: true,
+          headerStyle: {
+            backgroundColor: "#181818",
+          },
+          headerTintColor: "#fff",
+          title: "Log in",
+          headerLeft: () => (
+            <Link href="/(auth)/welcome">
+              <Image className="size-7" source={images.left} />
+            </Link>
+          ),
+        }}
+      />
       <Stack.Screen name="register" options={{ headerShown: false }} />
     </Stack>
   );
--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -1,9 +1,26 @@
-import { View, Text } from "react-native";
-
+import { View, Text, TextInput, TouchableOpacity } from "react-native";
 const Login = () => {
   return (
-    <View className="flex-1 flex items-center justify-center">
-      <Text>Login Page</Text>
+    <View className="bg-[#181818] min-h-screen p-6 flex">
+      <View>
+        <Text className="text-3xl font-semibold text-white">
+          Email or username
+        </Text>
+        <TextInput className="bg-[#717171] rounded-md h-14 px-5 font-semibold flex items-center text-white mt-1" />
+      </View>
+      <View className="mt-10">
+        <Text className="text-3xl font-semibold text-white">Password</Text>
+        <TextInput className="bg-[#717171] rounded-md h-14 px-5 font-semibold flex items-center text-white mt-1" />
+      </View>
+
+      <View className="flex items-center mt-16 gap-5">
+        <TouchableOpacity className="rounded-full  bg-white text-black px-11 py-5 ">
+          <Text className="text-xl font-bold ">Log in</Text>
+        </TouchableOpacity>
+        <TouchableOpacity className="rounded-full  border border-[#686868] px-7 py-2 ">
+          <Text className="text-white">Log in without password</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
